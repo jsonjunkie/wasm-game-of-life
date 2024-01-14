c9c7ad7e40afb68bd335ff182244a1cb4ac0fbc6
Beau Reescano <25470092+jsonjunkie@users.noreply.github.com>
2024-01-13 22:15:45 -0600

    fixed bug

--- src/lib.rs
+++ src/lib.rs
@@ -33,7 +33,7 @@ impl Universe {
                 let neighbor_row = (row + delta_row) % self.height;
                 let neighbor_col = (column + delta_col) % self.width;
                 let idx = self.get_index(neighbor_row, neighbor_col);
-                count += self.cells[idx] as u8
+                count += self.cells[idx] as u8;
             }
         }
         count
@@ -42,63 +42,67 @@ impl Universe {
 
 #[wasm_bindgen]
 impl Universe {
-   pub fn tick(&mut self) {
-       let mut next = self.cells.clone();
+    pub fn tick(&mut self) {
+        let mut next = self.cells.clone();
 
-       for row in 0..self.height {
-           for col in 0..self.width {
-               let idx = self.get_index(row,col);
-               let cell = self.cells[idx];
-               let live_neighbors = self.live_neighbor_count(row, col);
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let idx = self.get_index(row, col);
+                let cell = self.cells[idx];
+                let live_neighbors = self.live_neighbor_count(row, col);
 
-               let next_cell = match (cell, live_neighbors) {
-                   // Rule 1: Any live cell with fewer than two live neighbours
-                   // dies, as if caused by underpopulation.
-                   (Cell::Alive, x) if x < 2 => Cell::Dead,
-                   
-                   // Rule 2: Any live cell with two or three live neighbors
-                   // lives on to the next generation.
-                   (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                let next_cell = match (cell, live_neighbors) {
+                    // Rule 1: Any live cell with fewer than two live neighbours
+                    // dies, as if caused by underpopulation.
+                    (Cell::Alive, x) if x < 2 => Cell::Dead,
 
-                   // Rule 3: Any live cell with more than three live neighbors
-                   // dies, as if by overpopulation.
-                   (Cell::Alive, x) if x > 3 => Cell::Dead,
+                    // Rule 2: Any live cell with two or three live neighbors
+                    // lives on to the next generation.
+                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
 
-                   // Rule 4: Any dead cell with exactly three live neighbours
-                   // becomes a live cell, as if by reproduction.
-                   (Cell::Dead, 3) => Cell::Alive,
+                    // Rule 3: Any live cell with more than three live neighbors
+                    // dies, as if by overpopulation.
+                    (Cell::Alive, x) if x > 3 => Cell::Dead,
 
-                   // All other cells remain in the same state.
-                   (otherwise, _) => otherwise,
-               };
+                    // Rule 4: Any dead cell with exactly three live neighbours
+                    // becomes a live cell, as if by reproduction.
+                    (Cell::Dead, 3) => Cell::Alive,
 
-               next[idx] = next_cell;
-           }
-       }
-   } 
+                    // All other cells remain in the same state.
+                    (otherwise, _) => otherwise,
+                };
 
-   pub fn new() -> Universe {
-       let width = 64;
-       let height = 64;
+                next[idx] = next_cell;
+            }
+        }
 
-       let cells = (0..width * height).map(|i| {
-           if i% 2 == 0 || i % 7 == 0 {
-               Cell::Alive
-           } else {
-               Cell::Dead
-           }
-       }).collect();
+        self.cells = next;
+    }
 
-       Universe {
-           width,
-           height,
-           cells,
-       }
-   }
+    pub fn new() -> Universe {
+        let width = 64;
+        let height = 64;
 
-   pub fn render(&self) -> String {
-       self.to_string()
-   }
+        let cells = (0..width * height)
+            .map(|i| {
+                if i % 2 == 0 || i % 7 == 0 {
+                    Cell::Alive
+                } else {
+                    Cell::Dead
+                }
+            })
+            .collect();
+
+        Universe {
+            width,
+            height,
+            cells,
+        }
+    }
+
+    pub fn render(&self) -> String {
+        self.to_string()
+    }
 }
 
 use std::fmt;
@@ -115,5 +119,4 @@ impl fmt::Display for Universe {
 
         Ok(())
     }
-    
 }
